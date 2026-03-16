19e3e191d33341a4e5ae9124cd50c38f1f5e6745
myIndex <231722259+denyssemenchuk-source@users.noreply.github.com>
2026-03-16 14:58:51 +0200

    Lab JS

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Happy developing ✨')
+let a=5;
+let b=8;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+const first_name ="Semenchuk ";
+const last_name= "Denis";
+let full_name = first_name+last_name;
+console.log(full_name);
+
+const age = 18;
+if(age >= 18){
+    console.log("Доступ Дозволено");
+}
+else{
+    console.log("Доступ Заборонено");
+}
+
+for(let i=0; i<=10; i++){
+    console.log(i);
+}
+
+
+function square(number){
+    return number*number;
+}
+console.log(square(5));
+
+const fruits = ["banana", "apple", "cherry"];
+fruits.push("watermelon");
+for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
